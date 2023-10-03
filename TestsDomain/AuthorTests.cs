e2426e02fab77c5db198e49b600bfd3e7c0cbdf5
MarinaPaley <marina_paley@mail.ru>
2023-10-03 18:06:17 +0300

    Начали писать тесты на Автора.

---
+++ TestsDomain/AuthorTests.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="AuthorTests.cs" company="Васильева М.А.">
+// Copyright (c) Васильева М.А.. All rights reserved.
+// </copyright>
+
+namespace TestsDomain
+{
+    using Domain;
+
+    /// <summary>
+    /// Класс тестов на <see cref="Author"/>.
+    /// </summary>
+    [TestFixture]
+    public class AuthorTests
+    {
+        [Test]
+        public void Ctor_ValidData_Success()
+        {
+            Assert.DoesNotThrow(
+                () => _ = new Author(name: "Лев", familyName: "Толстой", surName: "Николаевич"));
+        }
+    }
+}
