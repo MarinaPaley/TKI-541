686dd78850f6ef3427e6917485d05affc8b4a145
MarinaPaley <marina_paley@mail.ru>
2023-10-04 10:09:29 +0300

    Начали работу над произведением

---
+++ Domain/Manuscript.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Manuscript.cs" company="Васильева М.А.">
+// Copyright (c) Васильева М.А.. All rights reserved.
+// </copyright>
+
+namespace Domain
+{
+    using System;
+
+    public class Manuscript : IEquatable<Manuscript>
+    {
+        public Guid Id { get; }
+        public string Title { get; init; }
+        public Author Author { get; init; }
+        public bool Equals(Manuscript? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
