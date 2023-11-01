5074d759be75b3774d2a5e9ce28d69b3dd07216c
MarinaPaley <marina_paley@mail.ru>
2023-11-01 10:31:26 +0300

    Начало "Полка"

--- Domain/Book.cs
+++ Domain/Book.cs
@@ -12,59 +12,65 @@ namespace Domain
     /// </summary>
     public sealed class Book : IEquatable<Book>
     {
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Book"/>.
+        /// </summary>
+        /// <param name="title">Название книги. </param>
+        /// <param name="publishing">Издательство. </param>
+        /// <param name="manuscripts"> Рукопись. </param>
+        /// <param name="pageCount">Количество страниц. </param>
         public Book(string title, Publishing publishing, ISet<Manuscript> manuscripts, int pageCount)
         {
             this.Id = Guid.NewGuid();
-            Title = title;
-            Publishing = publishing;
-            Manuscripts = manuscripts;
-            PageCount = pageCount;
+            this.Title = title;
+            this.Publishing = publishing;
+            this.Manuscripts = manuscripts;
+            this.PageCount = pageCount;
         }
 
         /// <summary>
-        /// 
+        /// Идентификатор.
         /// </summary>
         public Guid Id { get; }
 
         /// <summary>
-        /// 
+        /// Название.
         /// </summary>
         public string Title { get; init; }
 
         /// <summary>
-        /// 
+        /// Издательство.
         /// </summary>
         public Publishing Publishing { get; init; }
 
         /// <summary>
-        /// 
+        /// Рукопись.
         /// </summary>
         public ISet<Manuscript> Manuscripts { get; init; } = new HashSet<Manuscript>();
 
         /// <summary>
-        /// 
+        /// Количество страниц.
         /// </summary>
         public int PageCount { get; init; }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
-        /// <exception cref="NotImplementedException"></exception>
+        /// <inheritdoc/>
         public bool Equals(Book? other)
         {
-            throw new NotImplementedException();
+            if (ReferenceEquals(null, other) && other is null)
+            {
+                return false;
+            }
+
+            return this.Title.Equals(other.Title)
+                && this.Manuscripts.SetEquals(other.Manuscripts);
         }
 
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as Book);
-        }
+        /// <inheritdoc/>
+        public override bool Equals(object? obj) =>
+            this.Equals(obj as Book);
 
-        public override int GetHashCode()
-        {
-            throw new NotImplementedException();
-        }
+        /// <inheritdoc/>
+        public override int GetHashCode() =>
+            this.Title.GetHashCode() * this.Manuscripts.GetHashCode();
     }
 }
---
+++ Domain/Shelf.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="Shelf.cs" company="Васильева М.А.">
+// Copyright (c) Васильева М.А.. All rights reserved.
+// </copyright>
+
+namespace Domain
+{
+    public class Shelf : IComparable<Shelf>
+    {
+        public int CompareTo(Shelf? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
