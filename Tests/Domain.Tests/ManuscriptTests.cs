7ed3e3e4efd2ba63606604b65d3069b2e4341d2c
MarinaPaley <marina_paley@mail.ru>
2023-11-15 01:22:30 +0300

    Внесены мелки правки в классы тестов

--- Tests/Domain.Tests/ManuscriptTests.cs
+++ Tests/Domain.Tests/ManuscriptTests.cs
@@ -1,7 +1,8 @@
 ﻿// <copyright file="ManuscriptTests.cs" company="Васильева М.А.">
 // Copyright (c) Васильева М.А.. All rights reserved.
 // </copyright>
-namespace TestsDomain
+
+namespace Domain.Tests
 {
     using System;
     using Domain;
@@ -9,7 +10,7 @@ namespace TestsDomain
 
     [TestFixture]
     [Ignore("Логика пока не реализована.")]
-    public class ManuscriptTests
+    public sealed class ManuscriptTests
     {
         [Test]
         public void Ctor_Valid_Success()
